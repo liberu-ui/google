5bad20095d487373a3a50f866bbcb22bff1d6402
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-09-03 09:59:18 +0300

    fixes missing icon import

--- src/icons.js
+++ src/icons.js
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+import { faPuzzlePiece, faUserCog } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faGoogle, faPuzzlePiece);
+library.add(faGoogle, faPuzzlePiece, faUserCog);
