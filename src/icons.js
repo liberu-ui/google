27f2115c287f3dc43f45f94e97812228d6776e52
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:24:59 +0000

    Replace JavaScript with TypeScript

--- src/icons.js
+++
@@ -1,5 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { faPuzzlePiece, faUserCog } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faGoogle, faPuzzlePiece, faUserCog);
